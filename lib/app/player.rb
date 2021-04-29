c73e0ceaab20bb33f4819ae93237414622ef1641
Thom Tiber <contact@thomdeveloppeur.com>
2021-04-30 00:08:26 +0200

    Update player.rb

--- lib/app/player.rb
+++ lib/app/player.rb
@@ -1,8 +1,12 @@
+#player.rb
 class Player
-    attr_accessor :name , :symbole
-
-    def initialize(name, symbole)
-        @name = name
-        @symbole = symbole
-    end
+  attr_reader :player_name, :player_value
+  
+  def initialize
+    puts "Hello, please enter your player name:"  
+    @player_name = gets.chomp
+    puts "All right #{@player_name}, now choose a symbol:"
+    @player_value = gets.chomp
+    puts
+  end
 end
