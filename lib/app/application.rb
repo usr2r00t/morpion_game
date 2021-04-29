377ec9c9f8de84d98bd06711ad3f3b9dd0b88f62
Thom Tiber <contact@thomdeveloppeur.com>
2021-04-29 23:59:45 +0200

    Update application.rb

--- lib/app/application.rb
+++ lib/app/application.rb
@@ -1,5 +1,37 @@
+#Tic Tac Toe
 class Application
-    
-    def initialize
+
+  def self.perform
+      puts "████████╗██╗░█████╗░  ████████╗░█████╗░░█████╗░  ████████╗░█████╗░███████╗".blue
+      puts "╚══██╔══╝██║██╔══██╗  ╚══██╔══╝██╔══██╗██╔══██╗  ╚══██╔══╝██╔══██╗██╔════╝".blue
+      puts "░░░██║░░░██║██║░░╚═╝  ░░░██║░░░███████║██║░░╚═╝  ░░░██║░░░██║░░██║█████╗░░".blue
+      puts "░░░██║░░░██║██║░░██╗  ░░░██║░░░██╔══██║██║░░██╗  ░░░██║░░░██║░░██║██╔══╝░░".blue
+      puts "░░░██║░░░██║╚█████╔╝  ░░░██║░░░██║░░██║╚█████╔╝  ░░░██║░░░╚█████╔╝███████╗".blue
+      puts "░░░╚═╝░░░╚═╝░╚════╝░  ░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░  ░░░╚═╝░░░░╚════╝░╚══════╝".blue
+      puts
+      puts "█▀█ █░█ █▄▄ █▄█".center(70).red 
+      puts "█▀▄ █▄█ █▄█ ░█░".center(70).red
+      puts
+      puts
+      puts "/_\\Write the boxes in this format for playing -> A1/B1/..".red
+      puts
+    game = Game.new
+    count = 1
+      
+    while(true)
+      game.turn(game.player1)
+      count += 1
+        
+      if (game.board.victory? == true) || count >= 9
+        break
+      end
+        
+      game.turn(game.player2)
+      count += 1
+        
+      if (game.board.victory? == true) || count >= 9
+        break
+      end
     end
+  end
 end
