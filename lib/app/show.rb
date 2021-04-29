bf971bda6354a10130c8c659d560d375deeb2730
Thom Tiber <contact@thomdeveloppeur.com>
2021-04-30 00:09:07 +0200

    Update show.rb

--- lib/app/show.rb
+++ lib/app/show.rb
@@ -1,5 +1,14 @@
+#show.rb  
 class Show
-
-    def initialize
-    end
+  
+  def show_board(board)
+    board.grid.each.with_index do |cell, index|
+      if (index%3 == 0)
+        puts
+      end
+      print "|#{cell[1]}|"
+    end 
+    puts
+    return false
+  end
 end
